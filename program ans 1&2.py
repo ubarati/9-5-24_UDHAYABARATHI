c69c05d25676f229bee14abb79641613b1e08b93
ubarati <143688736+ubarati@users.noreply.github.com>
2024-05-09 09:53:37 +0530

    Add files via upload

---
+++ program ans 1&2.py
@@ -0,0 +1,12 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/DIVYAA/OneDrive/Desktop/720721115014_DIVYAA RANJANI/program1.py
+True
+True
+False
+>>> 
+= RESTART: C:/Users/DIVYAA/OneDrive/Desktop/720721115014_DIVYAA RANJANI/program2.py
+2
+1
+4
